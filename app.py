fc1debd0b2409d1b9c497a31b9f88e31aa50ec62
Jeramin Radesh Hillary <hillaryjeramin@gmail.com>
2022-10-12 00:00:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -282,15 +282,9 @@ def predict():
     Total_Premium_Str = str(Total_Premium)
     Total_Premium_Str
    
-    print(Total_Premium_Str)
+    print('Total_Premium_Str : ', Total_Premium_Str )
     return Total_Premium_Str, 200, {"Access-Control-Allow-Origin": "*"}
-
-    # sepal_length = float(request.form['sepal_length'])
-    # sepal_width = float(request.form['sepal_width'])
-    # petal_length = float(request.form['petal_length'])
-    # petal_width = float(request.form['petal_width'])
-    # result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
-    # return render_template('index.html', **locals())
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
